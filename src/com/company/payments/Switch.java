c5144cf6a771f134def10a0ec24717dbd6d4629e
balasivarathri <77465585+balasivarathri@users.noreply.github.com>
2022-01-06 23:48:19 +0200

    bala analyst 1st commit

--- src/com/company/payments/Switch.java
+++ src/com/company/payments/Switch.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Switch {
     public static void main(String[] args) {
         /*.........Write a java program to print a value by using switch statement...........*/
-        /*int number = 10;
+        int number = 90;
         switch (number) {
             case 10:
                 System.out.println("= 10");
@@ -18,9 +18,9 @@ public class Switch {
                 break;
             default:
                 System.out.println("None of the above");
-        }*/
+        }
         /*.........Write a java program to print a value by using switch statement...........*/
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         System.out.print("Please enter a number: ");
         int number = sc.nextInt();
         switch (number % 2) {
@@ -30,6 +30,6 @@ public class Switch {
             case 1:
                 System.out.println("Odd number");
                 break;
-        }
+        }*/
     }
 }
